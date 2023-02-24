8d885020dcb28213413fede82ab1de7361162656
6derick6 <6derick6@gmail.com>
2023-02-23 21:57:36 -0300

    Desenvolvendo Newsgram 1/3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import { db } from './firebase';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-function App() {
+function App() {    
+
+  const [user, setUser] = useState('Dérick');
 
   useEffect(()=>{
-    console.log(db);
+    
   },[])
 
 
   return (
     <div className="App">
      
+      <div className='header'>
+        <div className='center'>
+
+          <div className='header_logo'>
+            <a href=''><img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" /></a>
+          </div>
+
+          {
+            (user)?
+            <div>Olá Dérick</div>
+            :
+            <div className='header_loginForm'>
+              <form>
+                <input type="text" placeholder='Login...' />
+                <input type="password" placeholder='Senha...' />
+                <input type="submit" name='acao' value='Logar!' />
+              </form>
+            </div>
+          }
+
+        </div>
+      </div>
+
     </div>
   );
 }
